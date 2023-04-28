8adf839968bf436b28f56757f376120b071b9ee3
UnnatiSam <132055990+UnnatiSam@users.noreply.github.com>
2023-04-28 16:14:29 +0530

    Create area-of-rec.cpp

---
+++ area/rec/area-of-rec.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int l,b;
+	cin>>l>>b;
+	cout<<"two numbers "<<l<<" "<<b;
+	cout<<endl;
+	cout<<"area is ";
+	int a = l+b;
+	cout<<a;
+	
+}
